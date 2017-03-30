b1af8b94cc4cfb8efc46f1ed0f0194ecab68e8cf
Steve Robinson <steve.rob@me.com>
2017-03-30 16:35:59 +0530

    added remembnerable module back

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable
+  devise :database_authenticatable, :trackable, :rememberable
   belongs_to :account
   has_many :entries
 
